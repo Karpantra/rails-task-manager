157f491aeb4784662e0c6d221b263707f15ad820
Gautier Weigert <gautier.weigert@gmail.com>
2017-05-16 13:02:22 +0200

    task manager done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(name: "controller", detail: "générer les méthodes")
+Task.create(name: "views", detail: "générer le html")
+Task.create(name: "links", detail: "générer les liens")
